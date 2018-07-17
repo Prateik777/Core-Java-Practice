27db95a9f87acb69040c182170b1534cd3f78f22
Prateik777 <pg6276@gmail.com>
2018-07-17 21:21:39 +0530

    Add files via upload

---
+++ iterator_demo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class iterator_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList l = new ArrayList();
+		for(int i = 0; i <= 10; i++){
+			l.add(i);
+		}
+		System.out.println(l);
+		
+		Iterator itr = l.iterator();
+		while(itr.hasNext()){
+			Integer I = (Integer)itr.next();
+			if(I % 2 == 0){
+				System.out.println(I);
+			}
+			else
+				itr.remove();
+		}
+	}
+}
